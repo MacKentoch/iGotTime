025dc4e8f193525db5b0d83ec591a8b02306ee98
MacKentoch <erwan.datin@gmail.com>
2015-09-18 13:40:11 +0200

    added gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var sass         = require('gulp-sass');
 var cssmin       = require('gulp-cssmin');
 var notify 			 = require('gulp-notify');
 var rename			 = require('gulp-rename');
+var concat 			 = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 var exec         = require('child_process').exec;
 var gulpConfig   = require('./gulpConfig');
@@ -66,9 +67,9 @@ gulp.task('connect', function() {
 	// 	.pipe(gulp.dest(gulpConfig.destDirs.app.css, { cwd: gulpConfig.base.root }));
 
 	//not minified
-	gulp.src(gulpConfig.srcFiles.app.stepway.sass, { cwd: gulpConfig.base.root })
+	gulp.src(gulpConfig.bases.src + gulpConfig.components.iGotTime.css, { cwd: gulpConfig.bases.root })
 		.pipe(sass().on('error', notify.onError(function (error) { return 'Error: ' + error.message;})))
-		.pipe(concat(gulpConfig.destFiles.app.stepway.css))
+		.pipe(concat(gulpConfig.components.iGotTime.bundleCssName))
     .pipe(rename(gulpConfig.components.iGotTime.bundleCssName, {extname: '.css'}))      
 		.pipe(gulp.dest(gulpConfig.bases.src + gulpConfig.components.iGotTime.cssDir));
 		
